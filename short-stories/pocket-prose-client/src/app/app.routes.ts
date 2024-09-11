1aadc80ee297f3110d88a01cebc2411e397be7e0
CasColucci <crpadgitt@gmail.com>
2024-09-11 15:55:54 -0400

    A few more changes, pages, and routing

--- short-stories/pocket-prose-client/src/app/app.routes.ts
+++ short-stories/pocket-prose-client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './pages/search/search-page.component';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { ReadPageComponent } from './pages/read/read-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'search', component: SearchPageComponent},
+  {path: 'read', component: ReadPageComponent}
+];
